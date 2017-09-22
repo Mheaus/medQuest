93fa0935e82250857587fe13148205636d8d30d8
Romain Neuville <neuvilleromain@gmail.com>
2017-09-23 00:45:28 +0200

    work in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,55 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'lets create users'
+
+User.create!(
+  email: 'romain@gmail.com',
+  first_name: 'Romain',
+  password: '123456',
+)
+
+User.create!(
+  email: 'charles@gmail.com',
+  first_name: 'Charles',
+  password: '123456',
+)
+
+User.create!(
+  email: 'kos@gmail.com',
+  first_name: 'Kos',
+  password: '123456',
+)
+
+User.create!(
+  email: 'mathieu@gmail.com',
+  first_name: 'Mathieu',
+  password: '123456',
+)
+
+puts 'lets create badges'
+
+Badge.create!(
+  title: 'Le gastronome'
+)
+
+Badge.create!(
+  title: 'Le débutant'
+)
+
+Badge.create!(
+  title: 'Le randonneur'
+)
+
+Badge.create!(
+  title: 'Le cycliste'
+)
+
+Badge.create!(
+  title: 'Familial'
+)
+
+Badge.create!(
+  title: 'Découverte'
+)
