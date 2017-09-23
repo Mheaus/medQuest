a99a7275654fb049ab6cc335694052a0b62f5e1c
Mathieu Audebert <matheosmat@gmail.com>
2017-09-23 18:21:08 +0200

    badges#index

--- app/controllers/user_badges_controller.rb
+++ app/controllers/user_badges_controller.rb
@@ -1,7 +1,12 @@
 class UserBadgesController < ApplicationController
   def index
+    @user = current_user
+    @userbadges = @user.user_badges
   end
 
   def show
+    @user = current_user
+    @badge = UserBadge.find(params[:id]).badge
+    @objectives = @badge.objectives
   end
 end
