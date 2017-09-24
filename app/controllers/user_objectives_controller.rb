52ba5ccd567c5a96a14be6248f377f7204a0aece
Charles Pernet <charlespernet@gmail.com>
2017-09-24 14:12:33 +0200

    Trigger model if all objective done

--- app/controllers/user_objectives_controller.rb
+++ app/controllers/user_objectives_controller.rb
@@ -3,6 +3,7 @@ class UserObjectivesController < ApplicationController
     @userbadge = UserBadge.find(params[:user_badge_id])
     user_objective = @userbadge.user_objectives.build(user_badge_params)
     user_objective.save
+    @trigger_modal = @userbadge.all_objectives_done?
   end
 
   def user_badge_params
