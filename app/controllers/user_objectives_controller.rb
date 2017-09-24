8f590b9cc1ce6652ee3a17bd5d76afff116bc119
Charles Pernet <charlespernet@gmail.com>
2017-09-24 14:03:38 +0200

    Add Ajax request to create user_objective

--- app/controllers/user_objectives_controller.rb
+++ app/controllers/user_objectives_controller.rb
@@ -1,9 +1,8 @@
 class UserObjectivesController < ApplicationController
   def create
-    user_badge = UserBadge.find(params[:user_badge_id])
-    user_objective = user_badge.user_objectives.build(user_badge_params)
+    @userbadge = UserBadge.find(params[:user_badge_id])
+    user_objective = @userbadge.user_objectives.build(user_badge_params)
     user_objective.save
-    redirect_to user_badge
   end
 
   def user_badge_params
