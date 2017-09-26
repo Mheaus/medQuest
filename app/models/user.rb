d407110dfaf9d43fe0249f047a72764a779ea9ba
Romain Neuville <neuvilleromain@gmail.com>
2017-09-26 12:35:47 +0200

    Add UserBadge creation after User create

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :user_badges
-  
+
   has_many :ongoing_badges, -> { where(completed: false) }, :class_name => 'UserBadge'
   has_many :completed_badges, -> { where(completed: true) }, :class_name => 'UserBadge'
 
   has_many :user_objectives, through: :user_badges
+  after_create :add_badges
 
   def has_done_this?(objective)
     user_objectives.where(objective: objective).any?
   end
+
+  def add_badges
+    Badge.all.each do |badge|
+      UserBadge.create!(user: self, badge: badge)
+    end
+  end
+
 end
